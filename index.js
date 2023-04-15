d0d2580997a48e5686352aa2d9d0d8a5f149eace
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-04-15 03:41:20 +0200

    edit and pagination implemented

--- index.js
+++ index.js
@@ -18,8 +18,22 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/getEmployee', async (req, res) => {
-    const sql = "SELECT * from employee ORDER BY id DESC";
-    db.query(sql, (err, result) => {
+    const page = parseInt(req.query.page);
+    const size = parseInt(req.query.size);
+    console.log(page,size);
+    const sql = 'SELECT * from employee ORDER BY id DESC LIMIT ? OFFSET ?';
+    db.query(sql,[size, page*size], (err, result) => {
+        console.log(result);
+        const sql = "SELECT COUNT(*) as total_count FROM employee";
+        db.query(sql, (err, count) => {
+            res.send({ result, count })
+        })
+    })
+})
+app.get('/getEmployee/:id', async (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * from employee where id = ?";
+    db.query(sql, id, (err, result) => {
         res.send(result)
     })
 })
@@ -27,7 +41,7 @@ app.get('/getEmployee', async (req, res) => {
 app.delete('/deleteEmployee/:id', async (req, res) => {
     const id = req.params.id;
     const sql = "DELETE FROM employee where id = ?";
-    db.query(sql,id, (err, result) => {
+    db.query(sql, id, (err, result) => {
         const sql = "SELECT * from employee ORDER BY id DESC";
         db.query(sql, (err, result) => {
             res.send(result)
@@ -35,7 +49,32 @@ app.delete('/deleteEmployee/:id', async (req, res) => {
     })
 })
 
+app.put('/editEmployee/:id', async (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const job_title = req.body.job_title;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    const address = req.body.address;
+    const city = req.body.city;
+    const state = req.body.state;
+    const primary_contact = req.body.primary_contact;
+    const primary_phone = req.body.primary_phone;
+    const primary_relation = req.body.primary_relation;
+    const secondary_contact = req.body.secondary_contact;
+    const secondary_phone = req.body.secondary_phone;
+    const secondary_relation = req.body.secondary_relation;
+    const sql = "UPDATE employee SET name=?,job_title=?,phone=?,email=?,address=?,city=?,state=?,primary_contact=?,primary_phone=?,primary_relation=?,secondary_contact=?,secondary_phone=?,secondary_relation=? where id = ?";
+    db.query(sql, [name, job_title, phone, email, address, city, state, primary_contact, primary_phone, primary_relation, secondary_contact, secondary_phone, secondary_relation, id], (err, result) => {
+        const sql = "SELECT * from employee ORDER BY id DESC";
+        db.query(sql, (err, result) => {
+            res.send(result)
+        })
+    })
+})
 app.post('/addEmployee', async (req, res) => {
+    const page = parseInt(req.query.page);
+    const size = parseInt(req.query.size);
     console.log(req.body);
     const name = req.body.name;
     const job_title = req.body.job_title;
@@ -52,10 +91,14 @@ app.post('/addEmployee', async (req, res) => {
     const secondary_relation = req.body.secondary_relation;
     const sqlInsert = "INSERT INTO employee (name,job_title,phone,email,address,city,state,primary_contact,primary_phone,primary_relation,secondary_contact,secondary_phone,secondary_relation) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
     db.query(sqlInsert, [name, job_title, phone, email, address, city, state, primary_contact, primary_phone, primary_relation, secondary_contact, secondary_phone, secondary_relation], (err, result) => {
-        const sql = "SELECT * from employee ORDER BY id DESC";
-        db.query(sql, (err, result) => {
-            res.send(result)
+        const sql = 'SELECT * from employee ORDER BY id DESC LIMIT ? OFFSET ?';
+    db.query(sql,[size, page*size], (err, result) => {
+        console.log(result);
+        const sql = "SELECT COUNT(*) as total_count FROM employee";
+        db.query(sql, (err, count) => {
+            res.send({ result, count })
         })
+    })
     })
 })
 
